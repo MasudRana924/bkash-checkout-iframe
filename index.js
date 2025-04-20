d8576585a5c1bd6637abc90e59abc8a059f0a4fb
Masud Rana <masud.cse.dev@gmail.com>
2025-04-20 16:22:13 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const paymentRoutes = require('./routes/paymentRoutes');
+const paymentRoutes = require('./paymentRoutes');
 
 const app = express();
 
